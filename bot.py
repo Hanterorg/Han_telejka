1e67fb32aa7082ff36bbf1706fc6262267ab1e9c
Alex <90839295+Hanterorg@users.noreply.github.com>
2023-05-12 17:36:33 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,50 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('5939200730:AAHCpa51jpOx8FCIU_n5d8wERIiPxwGTmWU')
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("👋 Поздороваться")
+    btn2 = types.KeyboardButton("❓ Задать вопрос")
+    markup.add(btn1, btn2)
+    bot.send_message(message.chat.id,
+                     text="Привет, {0.first_name}! Я тестовый бот для демонстрации кнопок".format(
+                         message.from_user), reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def func(message):
+    if message.text == "👋 Поздороваться":
+        bot.send_message(message.chat.id, text="Привет)")
+    elif message.text == "❓ Задать вопрос":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Как меня зовут?")
+        btn2 = types.KeyboardButton("Что я могу?")
+        btn3 = types.KeyboardButton("Кто твой создатель?")
+        back = types.KeyboardButton("Вернуться в главное меню")
+        markup.add(btn1, btn2, btn3, back)
+        bot.send_message(message.chat.id, text="Задай мне вопрос", reply_markup=markup)
+
+    elif message.text == "Как меня зовут?":
+        bot.send_message(message.chat.id, "У меня нет имени..")
+
+    elif message.text == "Что я могу?":
+        bot.send_message(message.chat.id, text="Поздороваться)))")
+
+    elif message.text == "Кто твой создатель?":
+        bot.send_message(message.chat.id, text="Великий Александр")
+
+    elif message.text == "Вернуться в главное меню":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button1 = types.KeyboardButton("👋 Поздороваться")
+        button2 = types.KeyboardButton("❓ Задать вопрос")
+        markup.add(button1, button2)
+        bot.send_message(message.chat.id, text="Вы вернулись в главное меню", reply_markup=markup)
+    else:
+        bot.send_message(message.chat.id, text="Я тебя не понимаю(")
+
+
+bot.polling(none_stop=True)
